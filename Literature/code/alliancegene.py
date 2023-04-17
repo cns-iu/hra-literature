fca1d982249a7aa7b863fdd057e6805007f43f51
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-04-17 11:00:01 -0400

    Update alliancegene.py

--- Literature/code/alliancegene.py
+++ Literature/code/alliancegene.py
@@ -5,4 +5,4 @@ df = pd.read_csv('INTERACTION-GENE_HUMAN.csv', skiprows=14)
 reference_df = pd.DataFrame(columns=['Reference', 'Type'])
 reference_df['Reference'] = df['Publication Identifier(s)']
 reference_df['Type'] = 'genetic interaction'
-reference_df.to_csv('output3.csv', index=False)
+reference_df.to_csv('alliancegene_data.csv', index=False)
