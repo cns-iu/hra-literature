9251336d6372894d118b222f00a3eadfb0d3bfdb
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-07-26 12:13:52 -0400

    get_orcid_data

---
+++ experts/get_orcid_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a shell script to make a cURL request to fetch ORCID data in JSON format
+
+# The cURL command
+curl -L -H "Accept: application/json" https://orcid.org/0000-0002-3321-6137
