771234bbf95067d23665f29f11d2f8cc9096b4c4
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-08-12 16:11:23 -0400

    Update dataset_data_merged.py

--- KG/dataset_data_merged.py
+++ KG/dataset_data_merged.py
@@ -29,22 +29,26 @@ merged_datasets = pd.concat([gtex_datasets_renamed, hubmap_datasets_renamed, cxg
 # Replace NaN values with empty string
 merged_datasets_filled = merged_datasets.fillna('')
 
-#change into json format
-def aggregate_values_v2(x):
-    # If any value in the series is a string that looks like a list (starts with '[' and ends with ']'),
-    # return the first such value
-    for item in x:
-        if isinstance(item, str) and item.startswith('[') and item.endswith(']'):
-            return item
-    return list(set(x))
+# Save the modified dataset as a CSV file
+output_path_filled = 'C:\\Users\\yokong\\Desktop\HRALit KG\\datasets\\merged_datasets_filled.csv'
+merged_datasets_filled.to_csv(output_path_filled, index=False)
 
-# Grouping by 'dataset_id' and aggregating the rest of the columns with the new aggregation function
-agg_data_v2 = merged_datasets_filled.groupby('dataset_id').agg(aggregate_values_v2).reset_index()
+# #change into json format
+# def aggregate_values_v2(x):
+#     # If any value in the series is a string that looks like a list (starts with '[' and ends with ']'),
+#     # return the first such value
+#     for item in x:
+#         if isinstance(item, str) and item.startswith('[') and item.endswith(']'):
+#             return item
+#     return list(set(x))
 
-# Convert the updated dataframe to a JSON format
-json_data_v2 = agg_data_v2.to_json(orient='records')
+# # Grouping by 'dataset_id' and aggregating the rest of the columns with the new aggregation function
+# agg_data_v2 = merged_datasets_filled.groupby('dataset_id').agg(aggregate_values_v2).reset_index()
 
-# Save the transformed JSON data to a file
-json_file_path_v2 = 'C:\\Users\\yokong\\Desktop\HRALit KG\\datasets\\dataset_metadata_v2.json'
-with open(json_file_path_v2, 'w') as f:
-    f.write(json_data_v2)
+# # Convert the updated dataframe to a JSON format
+# json_data_v2 = agg_data_v2.to_json(orient='records')
+
+# # Save the transformed JSON data to a file
+# json_file_path_v2 = 'C:\\Users\\yokong\\Desktop\HRALit KG\\datasets\\dataset_metadata_v2.json'
+# with open(json_file_path_v2, 'w') as f:
+#     f.write(json_data_v2)
