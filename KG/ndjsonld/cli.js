9acc229b1a33fecd52f65efacac50c573a4520c1
Bruce W. Herr II <bherr@indiana.edu>
2023-08-18 02:19:41 -0400

    Code to process ndjson data and pipe through jsonld canonize to create n-quads

---
+++ KG/ndjsonld/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { createReadStream, createWriteStream, readFileSync } from 'fs';
+import ndjson from 'iterable-ndjson';
+import jsonld from 'jsonld';
+
+// Our CLI Program
+const program = new Command();
+
+program
+  .name('ndjsonld')
+  .argument('<ndjsonFile>')
+  .argument('<outputQuads>')
+  .option('-c, --context <contextFile>')
+  .action(async (inputFile, outputFile, { context }) => {
+    const source = inputFile === '-' ? process.stdin : createReadStream(inputFile, { autoClose: true });
+    const output = outputFile === '-' ? process.stdout : createWriteStream(outputFile, { autoClose: true });
+    const contextObject = JSON.parse(readFileSync(context))?.['@context'];
+
+    for await (const obj of ndjson.parse(source)) {
+      if (contextObject) {
+        obj['@context'] = contextObject;
+      }
+      const nquads = await jsonld.canonize(obj);
+      output.write(nquads);
+    }
+  });
+
+program.parse(process.argv);
