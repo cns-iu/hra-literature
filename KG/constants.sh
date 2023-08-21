6934519751fc01719f2ce97b8ea556bdce7988a5
Bruce W. Herr II <bherr@indiana.edu>
2023-08-21 19:06:40 -0400

    Added reporting and cleaning scripts

--- KG/constants.sh
+++ KG/constants.sh
@@ -1,5 +1,9 @@
 # File with constants that the scripts/*.sh use
 
 DATA_DIR='./data'
+REPORTS_DIR='./data/reports';
+BASENAME=$DATA_DIR/hra-lit
+
 GRAPH=https://purl.humanatlas.io/g/hra-lit/v0.1
 SAFE_MODE=--unsafe
+JNL=${BASENAME}.blazegraph.jnl
--- KG/scripts/30-convert-to-rdf.sh
+++ KG/scripts/30-convert-to-rdf.sh
@@ -4,14 +4,14 @@ shopt -s extglob
 set -ev
 
 # Canonize all json files adding the JSON-LD context and outputting a combined nquads file
-time cat data/*.json | ndjsonld canonize -c context.jsonld - $DATA_DIR/hra-lit.nq $SAFE_MODE
+time cat $DATA_DIR/*.json | ndjsonld canonize -c context.jsonld - ${BASENAME}.nq $SAFE_MODE
 
 # Load the nquads file into a blazegraph journal
-time blazegraph-runner load --journal=data/hra-lit.blazegraph.jnl \
+time blazegraph-runner load --journal=$JNL \
   --use-ontology-graph=false --graph="${GRAPH}" \
-  --informat=turtle $DATA_DIR/hra-lit.nq 
+  --informat=turtle ${BASENAME}.nq 
 
 # Dump the turtle version of the graph from blazegraph
-time blazegraph-runner dump --journal=$DATA_DIR/hra-lit.blazegraph.jnl \
+time blazegraph-runner dump --journal=$JNL \
   --graph="${GRAPH}" \
-  --outformat=turtle $DATA_DIR/hra-lit.ttl
+  --outformat=turtle ${BASENAME}.ttl
