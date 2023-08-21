4b367cfb3a7e926179a69a963b720bdae585b43e
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-08-21 01:14:03 -0400

    Update query_publication.sql

--- KG/sql/query_publication.sql
+++ KG/sql/query_publication.sql
@@ -1,67 +1,74 @@
 \copy (
-  WITH tmp AS (
+  WITH CTE AS (
     SELECT 
-        pub_34_organns.pmid,
+        'PMID/' || pub_34_organns.pmid AS identifier_id,
         pub_34_organns.uid,
-        normalize_id(doi) AS doi,
+        doi,
         pubyear,
-        normalize_id(article_title) AS article_title,
-        normalize_id(journal_title) AS journal_title,
-        normalize_id(identifier) AS author_id,
-        normalize_id(grant_id) AS grant_id,
-        normalize_id(organ) AS organ
+        article_title,
+        journal_title,
+        identifier AS author_id,
+        grant_id,
+        organ,
+	asct_ontology.id as ontology
     FROM pub_34_organns 
     LEFT JOIN pmid_author ON pub_34_organns.pmid = pmid_author.pmid 
     LEFT JOIN pmid_grant_all ON pub_34_organns.pmid = pmid_grant_all.pmid 
     LEFT JOIN pmid_34_organs ON pub_34_organns.pmid = pmid_34_organs.pmid 
+    LEFT JOIN asct_ontology on lower(pmid_34_organs.organ) = lower(asct_ontology.label)
     
     UNION 
     
     SELECT 
-        pub_34_organns.pmid,
+        'WOSID/' || pub_34_organns.uid  AS identifier_id,
         pub_34_organns.uid,
-        normalize_id(doi) AS doi,
+        doi,
         pubyear,
-        normalize_id(article_title) AS article_title,
-        normalize_id(journal_title) AS journal_title,
-        normalize_id(identifier) AS author_id,
-        normalize_id(grant_id) AS grant_id,
-        normalize_id(organ) AS organ
+        article_title,
+        journal_title,
+        identifier AS author_id,
+        grant_id,
+        organ,
+	asct_ontology.id as ontology
     FROM pub_34_organns 
     LEFT JOIN pmid_author ON pub_34_organns.pmid = pmid_author.pmid 
     LEFT JOIN uid_grant_all ON pub_34_organns.uid = uid_grant_all.uid 
     LEFT JOIN uid_34_organs ON pub_34_organns.pmid = uid_34_organs.pmid 
+    LEFT JOIN asct_ontology on lower(uid_34_organs.organ) = lower(asct_ontology.label)
     WHERE pub_34_organns.pmid IS NULL
 ), 
 
-CTE AS (
+CLEANED AS (
     SELECT 
-        pmid,
-        uid,
-        doi,
-        pubyear,
-        article_title,
-        journal_title,
-        ARRAY_AGG(DISTINCT author_id) AS author_ids,
-        ARRAY_AGG(DISTINCT grant_id) AS grant_ids,
-        ARRAY_AGG(DISTINCT organ) AS organs 
-    FROM tmp 
-    GROUP BY pmid, uid, doi, pubyear, article_title, journal_title
+        'Publication/' || normalize_id(identifier_id) as pub_id,
+        ARRAY_AGG(DISTINCT uid) FILTER (WHERE uid IS NOT NULL) AS uids,
+        ARRAY_AGG(DISTINCT doi) FILTER (WHERE doi IS NOT NULL) AS dois,
+        ARRAY_AGG(DISTINCT pubyear) FILTER (WHERE pubyear IS NOT NULL) AS pubyears,
+        ARRAY_AGG(DISTINCT article_title) FILTER (WHERE article_title IS NOT NULL) AS article_titles,
+        ARRAY_AGG(DISTINCT journal_title) FILTER (WHERE journal_title IS NOT NULL) AS journal_titles,
+        ARRAY_AGG(DISTINCT '#Author/' || normalize_id(author_id)) FILTER (WHERE author_id IS NOT NULL) AS author_ids,
+        ARRAY_AGG(DISTINCT '#Funding/' || normalize_id(grant_id)) FILTER (WHERE grant_id IS NOT NULL) AS grant_ids,
+        ARRAY_AGG(DISTINCT organ) FILTER (WHERE organ IS NOT NULL) AS organs,
+	ARRAY_AGG(DISTINCT '#Ontology/' || normalize_id(ontology)) FILTER (WHERE ontology IS NOT NULL) AS ontologies
+    FROM CTE 
+    GROUP BY pub_id
 )
 
 -- Final Selection
-SELECT ROW_TO_JSON(row) AS json_data 
+SELECT jsonb_strip_nulls(ROW_TO_JSON(row)::jsonb) AS json_data
 FROM (
     SELECT 
-        pmid,
-        uid,
-        doi,
-        pubyear,
-        article_title,
-        journal_title,
-        author_ids,
-        grant_ids,
-        organs 
-    FROM CTE
+        pub_id AS "@id",
+        'Publication' AS "@type", 
+        dois AS identifier, 
+        article_titles AS name,
+	journal_titles AS "journalName"
+        uids AS "WOSID",
+        pubyears AS "publicationYear",
+        author_ids AS "hasAuthor",
+        grant_ids AS "hasFunding", 
+	organs AS "organLabel"
+        ontologies AS "hasOrgan"
+    FROM CLEANED
   ) row 
 )TO '/N/slate/yokong/publication_metadata.json
