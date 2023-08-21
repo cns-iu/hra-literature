af58312e6e98c379f1b4582fd0fd8a65c49691fb
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-08-20 21:23:49 -0400

    Update query_creators.sql

--- KG/sql/query_creators.sql
+++ KG/sql/query_creators.sql
@@ -2,9 +2,9 @@
   WITH CTE AS (
     SELECT 
         creator_info.author_id,
-        full_name,
-	first_name,
-	last_name,
+        creator_info.full_name,
+	creator_info.first_name,
+	creator_info.last_name,
         version,
         creator_info.name,
         type,
@@ -12,9 +12,9 @@
 	asct_ontology.id as ontology,
 	predict_info.predicted_gender as predicted_gender,
 	predict_info.predicted_race as predicted_race,
-
+	author_info_lastest_affi.author_id as linked_author_id
     FROM creator_info
-    left join asct_expert on creator_info.author_id = asct_expert.author_id
+    left join author_info_lastest_affi on creator_info.author_id = author_info_lastest_affi.author_id
     left join asct_ontology on lower(creator_info.organ_name) = lower(asct_ontology.label)
     left join predict_info on creator_info.author_id = predict_info.author_id
 	where creator_info.author_id is not null
@@ -32,9 +32,13 @@
         ARRAY_AGG(DISTINCT name) FILTER (WHERE name IS NOT NULL) AS names,
         ARRAY_AGG(DISTINCT type) FILTER (WHERE type IS NOT NULL) AS types,
         ARRAY_AGG(DISTINCT organ_name) FILTER (WHERE organ_name IS NOT NULL) AS organ_names,
-	ARRAY_AGG(DISTINCT '#Ontology/' || normalize_id(ontology)) FILTER (WHERE ontology IS NOT NULL) AS ontologies
+	ARRAY_AGG(DISTINCT '#Ontology/' || normalize_id(ontology)) FILTER (WHERE ontology IS NOT NULL) AS ontologies,
+	CASE
+		WHEN linked_author_id IS NOT NULL THEN '#Author/' || normalize_id(linked_author_id)
+		ELSE NULL
+	END AS linked_author_id
     FROM CTE 
-    GROUP BY author_id, identifier, full_name, first_name, last_name, predicted_gender,predicted_race
+    GROUP BY author_id, identifier, full_name, first_name, last_name, predicted_gender,predicted_race,linked_author_id
   )
   SELECT 
 	jsonb_strip_nulls(ROW_TO_JSON(row)::jsonb) AS json_data
@@ -52,7 +56,8 @@
 	names as "DOName",
 	types as "DOType",
 	organ_names as "expertiseOrganLabel",
-	ontologies as "expertiseInOrgan"
+	ontologies as "expertiseInOrgan",
+	linked_author_id as "linkedToAuthor"
     FROM cleaned
 ) row
 )TO creators_metadata.json
