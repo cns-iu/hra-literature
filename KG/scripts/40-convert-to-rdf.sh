f2da13765b4f760bcfd52f0f3a3107b3e434af0d
Bruce W. Herr II <bherr@indiana.edu>
2023-08-21 19:29:41 -0400

    Split out canonize to its own step

--- KG/scripts/40-convert-to-rdf.sh
+++ KG/scripts/40-convert-to-rdf.sh
@@ -3,9 +3,6 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-# Canonize all json files adding the JSON-LD context and outputting a combined nquads file
-time cat $DATA_DIR/*.json | ndjsonld canonize -c context.jsonld - ${BASENAME}.nq $SAFE_MODE
-
 # Load the nquads file into a blazegraph journal
 time blazegraph-runner load --journal=$JNL \
   --use-ontology-graph=false --graph="${GRAPH}" \
