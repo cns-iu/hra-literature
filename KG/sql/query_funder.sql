1ea8d30f50c7f10890c470c51128d0cf4a1ca33e
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-08-20 22:23:57 -0400

    Update query_funder.sql

--- KG/sql/query_funder.sql
+++ KG/sql/query_funder.sql
@@ -1,12 +1,25 @@
 \copy (
   WITH CTE AS (
     SELECT 
-        '#Funder' || normalize_id(pmid_grant_all.agency) as agency_id,
-	ARRAY_AGG(DISTINCT agency) FILTER (WHERE agency IS NOT NULL) AS agencies,
-        ARRAY_AGG(DISTINCT country) FILTER (WHERE country IS NOT NULL) AS countries
+	agency,
+        country
     FROM pmid_grant_all
 	where pmid_grant_all.agency is not null
-	GROUP BY agency_id
+    UNION
+    SELECT
+	agency,
+	NULL
+    FROM pmid_grant_all
+	WHERE agency is not null
+	AND uid in (select uid from wosid_to_pmid where pmid is null)
+  ),
+  cleaned AS (
+    SELECT 
+        '#Funder' || normalize_id(agency) as agency_id,
+	ARRAY_AGG(DISTINCT agency) FILTER (WHERE agency IS NOT NULL) AS agencies,
+        ARRAY_AGG(DISTINCT country) FILTER (WHERE country IS NOT NULL) AS countries
+    FROM CTE
+    GROUP BY agency_id
   )
   SELECT 
 	jsonb_strip_nulls(ROW_TO_JSON(row)::jsonb) AS json_data
@@ -15,6 +28,6 @@
         agency_id AS "@id", 
         'Funder' AS "@type", 
         agencies AS identifier
-    FROM CTE
+    FROM cleaned
 ) row
 )TO funders_metadata.json
