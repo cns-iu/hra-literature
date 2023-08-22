a413c83d116173208aa638209c91064498bed1ec
Bruce W. Herr II <bherr@indiana.edu>
2023-08-22 16:17:52 -0400

    Add mega metagraph

--- KG/scripts/50-reports.sh
+++ KG/scripts/50-reports.sh
@@ -8,6 +8,12 @@ mkdir -p $REPORTS_DIR
 for query in sparql/*.rq; do
   qname=$(basename ${query%.rq})
   echo $qname
-  #blazegraph-runner select --journal=$JNL $query $REPORTS_DIR/${qname}.tsv
-  comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
+  TSV=$REPORTS_DIR/${qname}.tsv
+  CSV=$REPORTS_DIR/${qname}.csv
+  blazegraph-runner select --journal=$JNL $query $TSV
+  head -1 $TSV | perl -pe 's/\?//g' > $CSV
+  tail -n +2 $TSV | csvformat -t -D ',' -U 0 > $CSV
+  #comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
 done
+
+rm -f $REPORTS_DIR/*.tsv
