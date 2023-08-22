8ed0fd1d7a50119c2c05272d18e3a36916eab86d
Bruce W. Herr II <bherr@indiana.edu>
2023-08-22 15:38:50 -0400

    Add more graphs and network generation

--- KG/scripts/50-reports.sh
+++ KG/scripts/50-reports.sh
@@ -8,5 +8,6 @@ mkdir -p $REPORTS_DIR
 for query in sparql/*.rq; do
   qname=$(basename ${query%.rq})
   echo $qname
-  blazegraph-runner select --journal=$JNL $query $REPORTS_DIR/${qname}.tsv
+  #blazegraph-runner select --journal=$JNL $query $REPORTS_DIR/${qname}.tsv
+  comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
 done
