335cacc27c31e524742376a83dbbfaf75811cca1
Bruce W. Herr II <bherr@indiana.edu>
2023-08-22 15:38:50 -0400

    Add more graphs and network generation

--- KG/scripts/40-convert-to-rdf.sh
+++ KG/scripts/40-convert-to-rdf.sh
@@ -12,3 +12,17 @@ time blazegraph-runner load --journal=$JNL \
 time blazegraph-runner dump --journal=$JNL \
   --graph="${GRAPH}" \
   --outformat=turtle ${BASENAME}.ttl
+
+# Download the CCF.OWL KG
+CCF=https://purl.org/ccf/releases/2.2.1/ccf.owl
+curl -L -o $DATA_DIR/ccf.owl $CCF
+time blazegraph-runner load --journal=$JNL \
+  --use-ontology-graph=false --graph=$CCF \
+  --informat=rdfxml $DATA_DIR/ccf.owl
+
+# Download hubmap rui_locations.jsonld
+RUI=https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld
+curl -L $RUI | jsonld canonize > $DATA_DIR/hubmap-rui_locations.nq
+time blazegraph-runner load --journal=$JNL \
+  --use-ontology-graph=false --graph=$RUI \
+  --informat=turtle $DATA_DIR/hubmap-rui_locations.nq
