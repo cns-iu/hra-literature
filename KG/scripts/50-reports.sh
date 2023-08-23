a602c8af31df05ddeafc2530edf125755dfc886f
Bruce W. Herr II <bherr@indiana.edu>
2023-08-23 08:17:40 -0400

    Increase the timeout for local blazegraph so that it can run longer queries

--- KG/scripts/50-reports.sh
+++ KG/scripts/50-reports.sh
@@ -10,12 +10,15 @@ for query in sparql/*.rq; do
   echo $qname
   TSV=$REPORTS_DIR/${qname}.tsv
   CSV=$REPORTS_DIR/${qname}.csv
-  blazegraph-runner select --journal=$JNL $query $TSV
-  head -1 $TSV | perl -pe 's/\?//g' > $CSV
-  tail -n +2 $TSV | csvformat -t -D ',' -U 0 | \
-    perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#string\>//g' | \
-    perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#integer\>//g' > $CSV
-  #comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
+
+  ## Method using blazegraph runner, which requires no server, but is a bit wonky
+  # blazegraph-runner select --journal=$JNL $query $TSV
+  # head -1 $TSV | perl -pe 's/\?//g' > $CSV
+  # tail -n +2 $TSV | csvformat -t -D ',' -U 0 | \
+  #   perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#string\>//g' | \
+  #   perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#integer\>//g' > $CSV
+
+  comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
 done
 
 rm -f $REPORTS_DIR/*.tsv
