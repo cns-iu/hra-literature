f03de5b00fe2c9cbd65fdfc8ad201a57a97d5cc4
Bruce W. Herr II <bherr@indiana.edu>
2023-08-23 17:33:01 -0400

    Set default 'endpoint' to be blazegraph-runner

--- KG/scripts/50-reports.sh
+++ KG/scripts/50-reports.sh
@@ -11,14 +11,16 @@ for query in sparql/*.rq; do
   TSV=$REPORTS_DIR/${qname}.tsv
   CSV=$REPORTS_DIR/${qname}.csv
 
-  ## Method using blazegraph runner, which requires no server, but is a bit wonky
-  # blazegraph-runner select --journal=$JNL $query $TSV
-  # head -1 $TSV | perl -pe 's/\?//g' > $CSV
-  # tail -n +2 $TSV | csvformat -t -D ',' -U 0 | \
-  #   perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#string\>//g' | \
-  #   perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#integer\>//g' > $CSV
-
-  comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
+  if [ "${SPARQL_ENDPOINT}" == "blazegraph-runner" ]; then
+    ## Method using blazegraph runner, which requires no server, but is a bit wonky
+    blazegraph-runner select --journal=$JNL $query $TSV
+    head -1 $TSV | perl -pe 's/\?//g;s/\t/\,/g' > $CSV
+    tail -n +2 $TSV | csvformat -t -D ',' -U 0 | \
+      perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#string\>//g' | \
+      perl -pe 's/\^\^\<http\:\/\/www\.w3\.org\/2001\/XMLSchema\#integer\>//g' > $CSV
+  else
+    comunica-sparql $SPARQL_ENDPOINT -f $query -t text/csv > $REPORTS_DIR/${qname}.csv
+  fi
 done
 
 rm -f $REPORTS_DIR/*.tsv
