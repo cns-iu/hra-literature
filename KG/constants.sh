96575ca1c5f44d0071a52c5c308c9e50f2308297
Bruce W. Herr II <bherr@indiana.edu>
2023-08-23 08:17:40 -0400

    Increase the timeout for local blazegraph so that it can run longer queries

--- KG/constants.sh
+++ KG/constants.sh
@@ -14,4 +14,8 @@ GRAPH=https://purl.humanatlas.io/g/hra-lit/v0.1
 SAFE_MODE=
 JNL=${BASENAME}.blazegraph.jnl
 
+# Official LOD server instance
+#SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
+
+# Local server via `docker compose up`
 SPARQL_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
