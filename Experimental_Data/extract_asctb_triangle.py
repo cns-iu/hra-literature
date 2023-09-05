8cdc421f602fe60ad6a9c9f4aa7561a9520186ea
yokongkk <114939029+yokongkk@users.noreply.github.com>
2023-09-05 10:56:30 -0400

    Update extract_asctb_triangle.py

--- Experimental_Data/extract_asctb_triangle.py
+++ Experimental_Data/extract_asctb_triangle.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # Function to extract and structure data for different entities
-def extract_entity_data(entity_data, entity_type, organ_key, row_id):
+def extract_entity_data(entity_data, ontology_type, organ_key, row_id):
     table_data = []
 
     for entry in entity_data:
@@ -12,8 +12,8 @@ def extract_entity_data(entity_data, entity_type, organ_key, row_id):
         name = entry.get('name', '')
         b_type = entry.get('b_type', '')  # Specific to biomarkers
 
-        # Append to table with row_id and entity_type
-        table_data.append([row_id, id_value, rdfs_label, name, b_type, organ_key, entity_type])
+        # Append to table with row_id and ontology_type
+        table_data.append([row_id, id_value, rdfs_label, name, b_type, organ_key, ontology_type])
 
     return table_data
 
@@ -22,11 +22,11 @@ def extract_entity_data(entity_data, entity_type, organ_key, row_id):
 with open('ccf-asctb-all.json', 'r') as f:
     data = json.load(f)
 
-# Initialize empty list to hold data for the new table with row_id and entity_type
+# Initialize empty list to hold data for the new table with row_id and ontology_type
 table_with_row_id_data = []
 
-# Define a mapping for entity_type abbreviations
-entity_type_mapping = {'anatomical_structures': 'AS', 'cell_types': 'CT', 'biomarkers': 'B'}
+# Define a mapping for ontology_type abbreviations
+ontology_type_mapping = {'anatomical_structures': 'AS', 'cell_types': 'CT', 'biomarkers': 'B'}
 
 # Iterate through each organ key in the JSON data
 for organ_key in data.keys():
@@ -37,18 +37,18 @@ for organ_key in data.keys():
         row_number = entry.get('rowNumber', '')
         row_id = f"{organ_key}_{row_number}"  # Concatenating organ and rowNumber
 
-        for entity_type in ['anatomical_structures', 'cell_types', 'biomarkers']:
-            entity_data = entry.get(entity_type, [])
-            entity_type_abbr = entity_type_mapping.get(entity_type, '')
+        for ontology_type in ['anatomical_structures', 'cell_types', 'biomarkers']:
+            entity_data = entry.get(ontology_type, [])
+            ontology_type_abbr = ontology_type_mapping.get(ontology_type, '')
 
-            table_data = extract_entity_data(entity_data, entity_type_abbr, organ_key, row_id)
+            table_data = extract_entity_data(entity_data, ontology_type_abbr, organ_key, row_id)
             table_with_row_id_data.extend(table_data)
 
 # Create DataFrame and save it to CSV
-table_with_row_id_columns = ['row_id', 'id', 'rdfs_label', 'name', 'b_type', 'organ', 'entity_type']
+table_with_row_id_columns = ['row_id', 'id', 'rdfs_label', 'name', 'b_type', 'organ', 'ontology_type']
 table_with_row_id_df = pd.DataFrame(table_with_row_id_data, columns=table_with_row_id_columns)
 
 # Save to CSV
-output_path = 'asctb_triangle.csv'
+output_path = 'hralit_ontology_triangle.csv'
 table_with_row_id_df.to_csv(output_path, index=False)
 
